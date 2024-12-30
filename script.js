3abc8e5e8dce423d456433c08bd17e8ba4cec532
Balagani Venkata Naveen Kumar <165892891+naveenyadav143@users.noreply.github.com>
2024-12-30 22:22:40 +0530

    Update script.js change time

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Countdown Timer
 const countdown = document.getElementById('countdown');
-const targetDate = new Date('December 31, 2024 23:59:00').getTime();
+const targetDateIST = new Date('2024-12-31T18:29:00Z').getTime();
 
 function updateCountdown() {
     const now = new Date().getTime();
